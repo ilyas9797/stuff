42ad55b81eb577623b25fc801ed2db12fc844c3b
Ilias <ildar97.97@gmail.com>
2018-12-16 16:51:13 +0300

    C and asm

---
+++ Simple_letter_cipher/program.c
@@ -0,0 +1,12 @@
+extern void _read_key();
+extern void _encrypt_text();
+extern void _decrypt_text();
+
+int main()
+{
+    _read_key();
+    _encrypt_text();
+    _decrypt_text();
+
+    return 0;
+}
